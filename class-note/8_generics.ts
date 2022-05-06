e9141ba30d59352540951218bd9f12e9cd8786a7
zinny22 <hyejinsang@naver.com>
2022-05-06 23:48:27 +0900

    55강

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,5 +1,30 @@
-function logText<T>(text:T):T {
+// function logText<T>(text:T):T {
+//   console.log(text);
+//   return text;
+// }
+// logText<string>('하이');
+
+-
+
+// function logText(text: string) {
+//   console.log(text);
+//   text.split('').reverse().join('')
+//   return text;
+// }
+
+// function logNumber(num: number) {
+//   console.log(num);
+//   return num;
+// }
+//타입을 다르게 받기 위해서 중복적인 코드를 사용하는건 별로 좋지 않음 
+
+function logText(text: string | number) {
   console.log(text);
   return text;
 }
-logText<string>('하이');
+
+const a = logText('a');
+a.split('')
+
+// logText('하이');
+// const num = logNumber(10);
